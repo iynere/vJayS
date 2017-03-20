ab7031cf74be3fdcea79fa60c63b65a82b77777e
isar0se <itsr053@gmail.com>
2017-03-20 18:07:48 -0400

    XD

--- app/containers/Deck.jsx
+++ app/containers/Deck.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Container} from 'semantic-ui-react'
+import YouTubePlayer from 'react-youtube'
 
 export const Deck = props => (
-	<Container>
-		{/*<Player />*/}
-		{/*<Controls />*/}
-		{/*<Queue />*/}
-	</Container>
+  <Container>
+    {/*<Player />*/}
+    {/*<Controls />*/}
+    {/*<Queue />*/}
+  </Container>
 )
 
 // if we're reusing components, how do we render them with different information ?
