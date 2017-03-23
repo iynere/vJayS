4a6b1d40dbd090d504862a55dd5088f256af264a
Ariane Joy Nova <arianenova@gmail.com>
2017-03-23 16:54:06 -0400

    emoji rain

--- app/components/EmojiEffect.jsx
+++ app/components/EmojiEffect.jsx
@@ -30,16 +30,36 @@ export default class EmojiEffect extends Component {
  drawEmoji(emoji){
    let url=this.getUrl(emoji);
 
+   var animationDiv=document.createElement('div')
+   animationDiv.setAttribute("class", "emojiAnimation")
    var img=new Image(100,100);
    img.src=url;
+   animationDiv.appendChild(img)
 
-   document.getElementById('emojiEffect').appendChild(img)
 
+   let columns=["col1", "col2", "col3", "col4", "col5", "col6", "col7", "col8", "col9", "col10"]
+   let column=columns[Math.floor(Math.random()*10)+1]
+   document.getElementById(column).appendChild(animationDiv)
+
+   setTimeout(()=>{
+     document.getElementById(column).removeChild(animationDiv)
+   }, 5000)
  }
 
  render(){
    return (
-     <div className="emojiEffect" id="emojiEffect"></div>
+     <div className="emojiEffect" id="emojiEffect">
+       <div id="col1"></div>
+       <div id="col2"></div>
+       <div id="col3"></div>
+       <div id="col4"></div>
+       <div id="col5"></div>
+       <div id="col6"></div>
+       <div id="col7"></div>
+       <div id="col8"></div>
+       <div id="col9"></div>
+       <div id="col10"></div>
+     </div>
    )
  }
 }
