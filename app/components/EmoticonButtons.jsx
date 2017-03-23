c7c54c9e0d15d9d7e10fae710bfa93712548438c
Ariane Joy Nova <arianenova@gmail.com>
2017-03-23 17:19:25 -0400

    mobile view fix

--- app/components/EmoticonButtons.jsx
+++ app/components/EmoticonButtons.jsx
@@ -2,9 +2,18 @@ import React, { Component } from 'react'
 
 export default props => {
   return (
-    <div className="buttonsContainer">
-      <div className="emojiButton" onClick={()=>props.handleEmojiClick("alien")}><img src="/logos/alienemoji.png"/></div>
-      <div className="emojiButton" onClick={()=>props.handleEmojiClick("fire")}><img src="/logos/fireemoji.png"/></div>
+    <div className="emojiMobile">
+      <h1>How are you feeling?</h1>
+      <div className="buttonsContainer">
+        <button className="emojiButton" onClick={()=>props.handleEmojiClick("alien")}><img src="/logos/alienemoji.png"/></button>
+        <button className="emojiButton" onClick={()=>props.handleEmojiClick("fire")}><img src="/logos/fireemoji.png"/></button>
+      </div>
     </div>
   )
+  // return(
+  //   <div className="buttonsContainer">
+  //       <button className="emojiButton" onClick={()=>props.handleEmojiClick("alien")} style={{background: "url('/logos/alienemoji.png')"}}></button>
+  //       <button className="emojiButton" onClick={()=>props.handleEmojiClick("fire")} style={{background: "url('/logos/fireemoji.png')"}}></button>
+  //   </div>
+  // )
 }
