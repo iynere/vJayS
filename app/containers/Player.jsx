59f64d042c0734821b4c18badb33e94f557e96f4
isar0se <itsr053@gmail.com>
2017-03-24 11:14:27 -0400

    playback rate can chagne !!

--- app/containers/Player.jsx
+++ app/containers/Player.jsx
@@ -5,106 +5,105 @@ import YouTube from 'react-youtube'
 var socket = io(window.location.origin)
 
 class Player extends Component {
-  constructor(props) {
-    let Direction = props.direction
-    super(props)
-    this.state = {
-      [`video${Direction}`]: {}
-    }
-    
-    this.reinitializeVideo = this.reinitializeVideo.bind(this)
-    this.handleVideoReady = this.handleVideoReady.bind(this)
-    this.handleVideoPlay = this.handleVideoPlay.bind(this)
-    this.handleVideoPause = this.handleVideoPause.bind(this)
-    this.handlePlaybackRateChange = this.handlePlaybackRateChange.bind(this)
-  }
-  
-  componentDidMount() {
-    let Direction = this.props.direction,
-      videoToEmit = this.props.queue[0] ? this.props.queue[0].id.videoId : ''
-  }
-  
-  handleVideoReady(event) {
-    this.reinitializeVideo(event)
-    let Direction = this.props.direction,
-      videoToEmit = this.props.queue.length ? this.props.queue[0].id.videoId : ''
-    
-    event.target.playVideo()
-    setTimeout(() => {
-      event.target.pauseVideo()
-    }, 100)
-    // console.log('VIDEO THAT WE EMIT', videoToEmit)
-    socket.emit(`playerMounted${Direction}`, videoToEmit)
-  }
-  
-  handleVideoPlay(event) {
-    let Direction = this.props.direction,
-      newCueTime = event.target.getCurrentTime()
-    this.reinitializeVideo(event)
-    socket.emit(`playingVideo${Direction}`, newCueTime)
-  }
-  
-  handleVideoPause(event) {
-    let Direction = this.props.direction,
-      newCueTime = event.target.getCurrentTime()
-    this.reinitializeVideo(event)
-    socket.emit(`pausingVideo${Direction}`, newCueTime)
-  }
-  
-  handlePlaybackRateChange(event) {
-    let Direction = this.props.direction,
-      newRate = event.data
-    console.log(newRate)
-    this.reinitializeVideo(event)
-    socket.emit(`changingVideo${Direction}PlaybackRate`, newRate)
-  }
-  
-  reinitializeVideo(event) {
-    event.target.setPlaybackQuality('small')
-    let Direction = this.props.direction
-    // console.log(event.target)
-    
-    this.setState({
-      [`video${Direction}`]: event.target
-    })
-  }
+	constructor(props) {
+		let Direction = props.direction
+		super(props)
+		this.state = {
+			[`video${Direction}`]: {}
+		}
+		
+		// this.reinitializeVideo = this.reinitializeVideo.bind(this)
+		this.handleVideoReady = this.handleVideoReady.bind(this)
+		this.handleVideoPlay = this.handleVideoPlay.bind(this)
+		this.handleVideoPause = this.handleVideoPause.bind(this)
+		this.handlePlaybackRateChange = this.handlePlaybackRateChange.bind(this)
+	}
+	
+	componentDidMount() {
+		let Direction = this.props.direction,
+			videoToEmit = this.props.queue[0] ? this.props.queue[0].id.videoId : ''
+	}
+	
+	handleVideoReady(event) {
+		let Direction = this.props.direction,
+			videoToEmit = this.props.queue.length ? this.props.queue[0].id.videoId : ''
+		
+		setTimeout(() => {
+			event.target.pauseVideo()
+			event.target.setPlaybackQuality('small')
+		}, 100)
+		// console.log('VIDEO THAT WE EMIT', videoToEmit)
+		socket.emit(`playerMounted${Direction}`, videoToEmit)
+	}
+	
+	handleVideoPlay(event) {
+		let Direction = this.props.direction,
+			newCueTime = event.target.getCurrentTime()
+		// this.reinitializeVideo(event)
+		socket.emit(`playingVideo${Direction}`, newCueTime)
+	}
+	
+	handleVideoPause(event) {
+		let Direction = this.props.direction,
+			newCueTime = event.target.getCurrentTime()
+		// this.reinitializeVideo(event)
+		socket.emit(`pausingVideo${Direction}`, newCueTime)
+	}
+	
+	handlePlaybackRateChange(event) {
+		let Direction = this.props.direction,
+			newRate = event.data
+		console.log(newRate)
+		// this.reinitializeVideo(event)
+		socket.emit(`changingVideo${Direction}PlaybackRate`, newRate)
+	}
+	
+	// reinitializeVideo(event) {
+	// 	event.target.setPlaybackQuality('small')
+	// 	let Direction = this.props.direction
+	// 	// console.log(event.target)
+		
+	// 	this.setState({
+	// 		[`video${Direction}`]: event.target
+	// 	})
+	// }
 
-  render() {
-    let queue = this.props.queue,
-      playerOptions = {
-      width: window.innerWidth / 2,
-      // height: window.innerHeight - 130,
-      playerVars: {
-        autoplay: 1,
-        cc_load_policy: 0,
-        controls: 1,
-        disablekb: 1,
-        enablejsapi: 1,
-        fs: 0,
-        iv_load_policy: 3,
-        modestbranding: 1,
-        rel: 0,
-        showInfo: 0
-      }
-    }
-    
-    return (
-      <YouTube
-        videoId={queue.length ? queue[0].id.videoId : ''}
-        opts={playerOptions}
-        onReady={this.handleVideoReady}
-        onPlay={this.handleVideoPlay}
-        onPause={this.handleVideoPause}
-        onPlaybackRateChange={this.handlePlaybackRateChange}
-        onStateChange={this.reinitializeVideo}
-      />
-    )   
-  }
+	render() {
+		let queue = this.props.queue,
+			playerOptions = {
+			width: window.innerWidth / 2,
+			// height: window.innerHeight - 130,
+			playerVars: {
+				autoplay: 1,
+				cc_load_policy: 0,
+				controls: 1,
+				disablekb: 1,
+				enablejsapi: 1,
+				fs: 0,
+				iv_load_policy: 3,
+				modestbranding: 1,
+				rel: 0,
+				showInfo: 0
+			}
+		}
+		
+		return (
+			<YouTube
+				videoId={queue.length ? queue[0].id.videoId : ''}
+				opts={playerOptions}
+				onReady={this.handleVideoReady}
+				onPlay={this.handleVideoPlay}
+				onPause={this.handleVideoPause}
+				onPlaybackRateChange={this.handlePlaybackRateChange}
+				// onStateChange={this.reinitializeVideo}
+			/>
+		)   
+	}
 }
 
 const mapStateToProps = (state, ownProps) => ({
-  queue: state[`queue${ownProps.direction}`],
-  // player: state[`player${ownProps.direction}`]
+	queue: state[`queue${ownProps.direction}`],
+	// player: state[`player${ownProps.direction}`]
 })
 
 // const mapDispatchToprops = ()
--- server/start.js
+++ server/start.js
@@ -19,8 +19,8 @@ const pkg = require('APP')
 const app = express()
 
 if (!pkg.isProduction && !pkg.isTesting) {
-  // Logging middleware (dev only)
-  app.use(require('volleyball'))
+	// Logging middleware (dev only)
+	app.use(require('volleyball'))
 }
 
 // Pretty error prints errors all pretty.
@@ -33,58 +33,58 @@ prettyError.skipNodeFiles()
 prettyError.skipPackage('express')
 
 module.exports = app
-  // Session middleware - compared to express-session (which is what's used in the Auther workshop), cookie-session stores sessions in a cookie, rather than some other type of session store.
-  // Cookie-session docs: https://www.npmjs.com/package/cookie-session
-  .use(require('cookie-session') ({
-    name: 'session',
-    keys: [process.env.SESSION_SECRET || 'an insecure secret key'],
-  }))
+	// Session middleware - compared to express-session (which is what's used in the Auther workshop), cookie-session stores sessions in a cookie, rather than some other type of session store.
+	// Cookie-session docs: https://www.npmjs.com/package/cookie-session
+	.use(require('cookie-session') ({
+		name: 'session',
+		keys: [process.env.SESSION_SECRET || 'an insecure secret key'],
+	}))
 
-  // Body parsing middleware
-  .use(bodyParser.urlencoded({ extended: true }))
-  .use(bodyParser.json())
+	// Body parsing middleware
+	.use(bodyParser.urlencoded({ extended: true }))
+	.use(bodyParser.json())
 
-  // Authentication middleware
-  .use(passport.initialize())
-  .use(passport.session())
+	// Authentication middleware
+	.use(passport.initialize())
+	.use(passport.session())
 
-  // Serve static files from ../public
-  .use(express.static(resolve(__dirname, '..', 'public')))
+	// Serve static files from ../public
+	.use(express.static(resolve(__dirname, '..', 'public')))
 
-  // Serve our api - ./api also requires in ../db, which syncs with our database
-  .use('/api', require('./api'))
+	// Serve our api - ./api also requires in ../db, which syncs with our database
+	.use('/api', require('./api'))
 
-  // Send index.html for anything else.
-  .get('/*', (_, res) => res.sendFile(resolve(__dirname, '..', 'public', 'index.html')))
+	// Send index.html for anything else.
+	.get('/*', (_, res) => res.sendFile(resolve(__dirname, '..', 'public', 'index.html')))
 
-  // Error middleware interceptor, delegates to same handler Express uses.
-  // https://github.com/expressjs/express/blob/master/lib/application.js#L162
-  // https://github.com/pillarjs/finalhandler/blob/master/index.js#L172
-  .use((err, req, res, next) => {
-    console.error(prettyError.render(err))
-    finalHandler(req, res)(err)
-  })
+	// Error middleware interceptor, delegates to same handler Express uses.
+	// https://github.com/expressjs/express/blob/master/lib/application.js#L162
+	// https://github.com/pillarjs/finalhandler/blob/master/index.js#L172
+	.use((err, req, res, next) => {
+		console.error(prettyError.render(err))
+		finalHandler(req, res)(err)
+	})
 
 if (module === require.main) {
-  // Start listening only if we're the main module.
-  //
-  // https://nodejs.org/api/modules.html#modules_accessing_the_main_module
-  const server = app.listen(
-    process.env.PORT || 1337,
-    () => {
-      console.log(`--- Started HTTP Server for ${pkg.name} ---`)
-      const { address, port } = server.address()
-      const host = address === '::' ? 'localhost' : address
-      const urlSafeHost = host.includes(':') ? `[${host}]` : host
-      console.log(`Listening on http://${urlSafeHost}:${port}`)
-    }
-  )
+	// Start listening only if we're the main module.
+	//
+	// https://nodejs.org/api/modules.html#modules_accessing_the_main_module
+	const server = app.listen(
+		process.env.PORT || 1337,
+		() => {
+			console.log(`--- Started HTTP Server for ${pkg.name} ---`)
+			const { address, port } = server.address()
+			const host = address === '::' ? 'localhost' : address
+			const urlSafeHost = host.includes(':') ? `[${host}]` : host
+			console.log(`Listening on http://${urlSafeHost}:${port}`)
+		}
+	)
 
-  var io = socketio(server)
-  require('./sockets/controller.js')(io);
-  require('./sockets/mobile.js')(io);
-  require('./sockets/player.js')(io);
-  require('./sockets/output.js')(io);
+	var io = socketio(server)
+	require('./sockets/controller.js')(io);
+	require('./sockets/mobile.js')(io);
+	require('./sockets/player.js')(io);
+	require('./sockets/output.js')(io);
 }
 
 // This check on line 64 is only starting the server if this file is being run directly by Node, and not required by another file.
