f3e4442f03d0bf55dd1c7c17f0f25dd98279822f
Ariane Joy Nova <arianenova@gmail.com>
2017-03-24 14:43:09 -0400

    remove from queue at video end and play next

--- app/containers/Player.jsx
+++ app/containers/Player.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import YouTube from 'react-youtube'
 import {addToSet} from 'APP/app/reducers/set'
+import {removeFromQueue} from 'APP/app/utils/queue'
 
 var socket = io(window.location.origin)
 
@@ -12,7 +13,7 @@ class Player extends Component {
     this.state = {
       [`video${Direction}`]: {}
     }
-    
+
     // this.reinitializeVideo = this.reinitializeVideo.bind(this)
     this.handleVideoReady = this.handleVideoReady.bind(this)
     this.handleVideoPlay = this.handleVideoPlay.bind(this)
@@ -20,16 +21,16 @@ class Player extends Component {
     this.handlePlaybackRateChange = this.handlePlaybackRateChange.bind(this)
     this.handleVideoEnd = this.handleVideoEnd.bind(this)
   }
-  
+
   componentDidMount() {
     let Direction = this.props.direction,
       videoToEmit = this.props.queue[0] ? this.props.queue[0].id.videoId : ''
   }
-  
+
   handleVideoReady(event) {
     let Direction = this.props.direction,
       videoToEmit = this.props.queue.length ? this.props.queue[0].id.videoId : ''
-    
+
     setTimeout(() => {
       event.target.pauseVideo()
       event.target.setPlaybackQuality('small')
@@ -37,7 +38,7 @@ class Player extends Component {
     // console.log('VIDEO THAT WE EMIT', videoToEmit)
     socket.emit(`playerMounted${Direction}`, videoToEmit)
   }
-  
+
   handleVideoPlay(event) {
     let Direction = this.props.direction,
       newCueTime = event.target.getCurrentTime()
@@ -48,14 +49,14 @@ class Player extends Component {
     //  [`video${Direction}`]: event.target
     // })
   }
-  
+
   handleVideoPause(event) {
     let Direction = this.props.direction,
       newCueTime = event.target.getCurrentTime()
     // this.reinitializeVideo(event)
     socket.emit(`pausingVideo${Direction}`, newCueTime)
   }
-  
+
   handlePlaybackRateChange(event) {
     let Direction = this.props.direction,
       newRate = event.data
@@ -63,9 +64,9 @@ class Player extends Component {
     // this.reinitializeVideo(event)
     socket.emit(`changingVideo${Direction}PlaybackRate`, newRate)
   }
-  
+
   handleVideoEnd() {
-    
+
     // 1. save just the info we need to a set on localStorage:
     // videoId, title, thumbnailUrl, direction
     let video = this.state[`video${this.props.direction}`],
@@ -78,13 +79,15 @@ class Player extends Component {
     console.log(this.state)
     console.log('SET ITEM: ', setItem)
     this.props.addToSet(setItem)
+    console.log("props direction", this.props.direction)
+    this.props.removeFromQueue(0, `queue${this.props.direction}`)
   }
-  
+
   // reinitializeVideo(event) {
   //  event.target.setPlaybackQuality('small')
   //  let Direction = this.props.direction
   //  // console.log(event.target)
-    
+
   //  this.setState({
   //    [`video${Direction}`]: event.target
   //  })
@@ -108,7 +111,7 @@ class Player extends Component {
         showInfo: 0
       }
     }
-    
+
     return (
       <YouTube
         videoId={queue.length ? queue[0].id.videoId : ''}
@@ -119,7 +122,7 @@ class Player extends Component {
         onPlaybackRateChange={this.handlePlaybackRateChange}
         onEnd={this.handleVideoEnd}
       />
-    )   
+    )
   }
 }
 
@@ -131,6 +134,9 @@ const mapStateToProps = (state, ownProps) => ({
 const mapDispatchToProps = dispatch => ({
   addToSet: setItem => {
     dispatch(addToSet(setItem))
+  },
+  removeFromQueue: (videoId, direction) => {
+    dispatch(removeFromQueue(videoId, direction))
   }
 })
 
