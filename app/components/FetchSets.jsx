498eb0b143c5f4919181fa5e9be4af8135a4bcfa
Anna Calinawan <anna.pamela@gmail.com>
2017-03-27 10:21:40 -0400

    Add fetch sets component

--- app/components/FetchSets.jsx
+++ app/components/FetchSets.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
+import { List } from 'semantic-ui-react'
 import {fetchAllSetsFromDb} from '../reducers/set'
 
 export default class FetchSets extends Component {
@@ -10,6 +11,21 @@ export default class FetchSets extends Component {
 	render() {
 		return(
 			<h4>BOOP FETCHING SETS</h4>
+			//Link to sets
 		)
 	}
 }
+
+const mapStateToProps = (state) => {
+	return ({}) //UserID
+}
+
+const mapDispatchToProps = (dispatch) => ({
+	fetchAllSetsFromDb: (userId) => { dispatch(fetchAllSetsFromDb(userId)) }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(SaveSet)
+
+
+// component did mount?? or onEnterHook?
+// get sets -> list sets
