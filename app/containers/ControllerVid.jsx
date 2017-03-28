7a3f0ef4176165ff84fc19d0981aa04e3c040da6
Ariane Joy Nova <arianenova@gmail.com>
2017-03-28 16:19:03 -0400

    play/pause both

--- app/containers/ControllerVid.jsx
+++ app/containers/ControllerVid.jsx
@@ -9,6 +9,10 @@ export default class ControllerVid extends Component {
 
   constructor(){
     super()
+    this.play="\u25B6"
+    this.pause="\u23F8"
+    this.playing=0;
+    this.state={unicode: "\u25B6"}
     this.handleOpacitySlider= this.handleOpacitySlider.bind(this)
   }
 
@@ -24,14 +28,23 @@ export default class ControllerVid extends Component {
     socket.emit('skipVideoPressed', direction)
   }
 
+  handlePlayBoth(){
+    this.playing = this.playing === 0 ? 1 : 0
+    let unicode = this.playing === 0 ? this.play : this.pause
+    this.setState({unicode: unicode})
+    socket.emit('playBothPressed', this.playing)
+  }
+
   render() {
     // console.log("command current", this.props.command);
+
     return (
       <div className="djControls">
         <h2>Video Controls</h2>
         <div className="djControlButtons">
           <button onClick={()=>this.handleSkipVideo("Left")}>Skip Left</button>
           <div className="sliders">
+            <button onClick={()=>this.handlePlayBoth()}>{this.state.unicode}</button>
             <SliderComponent  handleChange={this.handleOpacitySlider}/>
             <p>Opacity</p>
             <SliderComponent  handleChange={this.handleVolumeSlider}/>
