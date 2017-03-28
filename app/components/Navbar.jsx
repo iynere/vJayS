8ebeb844876d24f4acfae47d257756a1e0bbf200
isar0se <itsr053@gmail.com>
2017-03-28 18:00:14 -0400

    fetchin playlists worx now :D

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -10,44 +10,50 @@ var socket = io(window.location.origin)
 
 export const Navbar = () => {
 
-  var buttonSpacingStyle = {
-    marginRight: "10px"
-  }
+	var buttonSpacingStyle = {
+		marginRight: "10px"
+	}
+	
+	var searchStyles = {
+		width: "500px"
+	}
 
-  return (
-    <Menu widths={3} inverted style={{width: "100%", margin: "0 auto"}}>
-      <Container fluid>
-      <Menu.Item style={{ width: "25%", marginLeft: "300px"}}> 
-        <Button basic color={"youtube"} inverted onClick={evt => {
-          socket.emit('clearVideos')
-          evt.preventDefault()
-          store.dispatch(clearQueue('queueLeft'))
-          store.dispatch(clearQueue('queueRight'))
-        }} style={buttonSpacingStyle}>Clear Queues</Button>
-      <SaveSetModal/>
-      </Menu.Item>
-      <Menu.Item style={{width: "50%"}}>
-        {/*<YouTubeSearch
-          apiKey='AIzaSyBOr-nJwESPXBlOSh-4-bf2R-ayOTUFVt4' // how to use .env on the front-end
-          maxResults='5'
-          placeHolder='<- search'
-          callback={results => {
-            store.dispatch(addToQueue(results[0], 'queueLeft'))
-          }}
-        />
-        <YouTubeSearch
-          apiKey='AIzaSyBOr-nJwESPXBlOSh-4-bf2R-ayOTUFVt4' // how to use .env on the front-end
-          maxResults='5'
-          placeHolder='search ->'
-          callback={results => {
-            // console.log(results[0])
-            store.dispatch(addToQueue(results[0], 'queueRight'))
-          }}
-        />*/}
-        <Input fluid placeholder="Search Youtube" icon="search" style={{margin: "5px 20px"}}/>
-      </Menu.Item>
-      <LoginLogout />
-      </Container>
-    </Menu>
-  )
+	return (
+		<Menu widths={3} inverted style={{width: "100%", margin: "0 auto"}}>
+			<Container fluid>
+			<Menu.Item style={{ width: "25%", marginLeft: "300px"}}> 
+				<Button basic color={"youtube"} inverted onClick={evt => {
+					socket.emit('clearVideos')
+					evt.preventDefault()
+					store.dispatch(clearQueue('queueLeft'))
+					store.dispatch(clearQueue('queueRight'))
+				}} style={buttonSpacingStyle}>Clear Queues</Button>
+			<SaveSetModal/>
+			</Menu.Item>
+			<Menu.Item style={{width: "50%"}}>
+				{/*
+				<YouTubeSearch
+					apiKey='AIzaSyBOr-nJwESPXBlOSh-4-bf2R-ayOTUFVt4' // how to use .env on the front-end
+					maxResults='5'
+					placeHolder='search ->'
+					callback={results => {
+						// console.log(results[0])
+						store.dispatch(addToQueue(results[0], 'queueRight'))
+					}}
+				/>*/}
+				<YouTubeSearch
+					apiKey='AIzaSyBOr-nJwESPXBlOSh-4-bf2R-ayOTUFVt4'
+					style={searchStyles}
+					placeHolder='Search YouTube'
+					callback={results => {
+						console.log('SEARCH RESULTS: ', results)
+						store.dispatch(addToQueue(results[0], 'queueRight'))
+						store.dispatch(addToQueue(results[0], 'queueLeft'))
+					}}
+				/>
+			</Menu.Item>
+			<LoginLogout />
+			</Container>
+		</Menu>
+	)
 }
