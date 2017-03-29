2bb26868529c4d08ab921cb7c4a45c55c9810184
isar0se <itsr053@gmail.com>
2017-03-29 14:32:41 -0400

    refactored playlist searchin

--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -1,15 +1,13 @@
 import {combineReducers} from 'redux'
 
 const rootReducer = combineReducers({
-  auth: require('./auth').default,
-  // playlists: require('./playlists').default, for pulling stuff off youtube
-  player: require ('./player').default,
-  queue: require('./queue').default,
-  set: require('./set').default,
-  sets: require('./sets').default,
-//effects: ?
-
-  command: require('./command').default
+	auth: require('./auth').default,
+	command: require('./command').default,
+	player: require ('./player').default,
+	playlists: require('./playlists').default,
+	queue: require('./queue').default,
+	set: require('./set').default,
+	sets: require('./sets').default
 })
 
 export default rootReducer
--- app/reducers/playlists.jsx
+++ app/reducers/playlists.jsx
@@ -1,12 +1,93 @@
 import axios from 'axios'
+import localStore from 'store'
+import {receiveQueue} from './queue'
 
-const fetchPlaylists = () => dispatch => {
-  console.log('fetching')
-  axios.get('https://www.googleapis.com/youtube/v3/channels?part=contentDetails&mine=true')
-    .then(result => {
-      console.log(result)
-    })
-    .catch(error => console.log(error))
+// CONSTANTS
+const RECEIVE_PLAYLISTS = 'RECEIVE_PLAYLISTS'
+	
+// INITIAL STATE
+const	initialState = {
+	userPlaylists: []
 }
 
-export default fetchPlaylists
+// REDUCER
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case RECEIVE_PLAYLISTS:
+			return Object.assign({}, state, {userPlaylists: action.userPlaylists})
+		default:
+			return state
+	}
+}
+
+// ACTION TYPES
+// export const receiveAccessToken = accessToken => ({
+// 	type: RECEIVE_TOKEN,
+// 	accessToken
+// })
+
+export const receiveUserPlaylists = userPlaylists => ({
+	type: RECEIVE_PLAYLISTS,
+	userPlaylists
+})
+
+// export const receiveSelectedPlaylist = selectedPlaylist => ({
+// 	type: RECEIVE_PLAYLIST,
+// 	selectedPlaylist
+// })
+
+// UTIL
+const fetchAccessToken = userId => axios.get(`/api/auth/users/${userId}`)
+
+const fetchPlaylist = (accessToken, playlistId) => axios.get(`https://www.googleapis.com/youtube/v3/playlistItems?access_token=${accessToken}&part=snippet&maxResults=50&playlistId=${playlistId}`)
+
+// ACTION CREATORS
+export const fetchUserPlaylists = userId => dispatch => {
+	fetchAccessToken(userId)
+		.then(res => {
+			let accessToken = res.data.accessToken
+			axios.get(`https://www.googleapis.com/youtube/v3/playlists?access_token=${accessToken}&part=snippet&mine=true`)
+				.then(res => {
+					dispatch(receiveUserPlaylists(res.data.items))
+				})
+		})
+		.catch(console.error)
+}
+
+export const fetchSelectedPlaylist = (userId, playlistId) => dispatch => {
+	fetchAccessToken(userId)
+		.then(res => {
+			let accessToken = res.data.accessToken
+			fetchPlaylist(accessToken, playlistId)
+			.then(res => {
+				dispatch(loadYoutubePlaylist(res.data.items))
+			})
+		})
+		.catch(console.error)
+}
+
+const loadYoutubePlaylist = playlistItems => dispatch => {
+	
+	let queueLeft = [], queueRight = []
+
+	localStore.set("queueLeft", [])
+	localStore.set("queueRight", [])
+
+	playlistItems.filter(item => item.snippet.thumbnails /*filters out videos that have been deleted but are still in playlists*/).forEach((item, index)=> {
+		let itemForQueue = {
+			id: {videoId: item.snippet.resourceId.videoId},
+			snippet: {
+				title: item.snippet.title,
+				thumbnails: {default: {url: item.snippet.thumbnails.default.url}}
+			}
+		}
+		index % 2 === 0 ? queueLeft.push(itemForQueue) : queueRight.push(itemForQueue)
+	})
+
+	localStore.set("queueLeft", queueLeft)
+	localStore.set("queueRight", queueRight)
+	dispatch(receiveQueue(queueLeft, 'queueLeft'))
+	dispatch(receiveQueue(queueRight, 'queueRight'))
+}
+
+export default reducer
