b8886d6589014fbea25da73d17b9687c068057f2
isar0se <itsr053@gmail.com>
2017-04-04 12:51:30 -0400

    workin on https

--- server/api.js
+++ server/api.js
@@ -4,10 +4,16 @@ const db = require('APP/db')
 const api = module.exports = require('express').Router()
 
 api
-  .get('/heartbeat', (req, res) => res.send({ok: true,}))
-  .use('/auth', require('./auth'))
-  .use('/users', require('./users'))
-  .use('/sets', require('./sets'))
+	.get('/heartbeat', (req, res) => res.send({ok: true,}))
+	.get('/.well-known/acme-challenge/:cert', (req,res) => { 
+		let id = req.params.cert 
+		let finalString = id + 'F543hO9NZzW07BMuUXS78UXkQjSreIbZakVx_d9_dAE' 
+		res.setHeader('content-type', 'text/plain'); 
+		res.send(finalString) 
+	})
+	.use('/auth', require('./auth'))
+	.use('/users', require('./users'))
+	.use('/sets', require('./sets'))
 
 // No routes matched? 404.
 api.use((req, res) => res.status(404).end())
